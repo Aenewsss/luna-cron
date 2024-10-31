1e2325bb289830d318aed6b3ad0fc506d687f8d2
aenamartinelli <62079043+Aenewsss@users.noreply.github.com>
2024-10-30 22:44:45 -0300

    Update whatsapp.js

--- whatsapp.js
+++ whatsapp.js
@@ -1,9 +1,13 @@
 export async function sendWpp(phone, title, description) {
-    await fetch(
-        'https://lunapp.vercel.app/api/whatsapp-reminder',
-        {
-            method: 'POST',
-            body: JSON.stringify({ phone, title, description }),
-            headers: { 'Content-Type': 'application/json' }
-        })
+    try {
+        await fetch(
+            'https://lunap.vercel.app/api/whatsapp-reminder',
+            {
+                method: 'POST',
+                body: JSON.stringify({ phone, title, description }),
+                headers: { 'Content-Type': 'application/json' }
+            })
+    } catch (error) {
+        console.error(error)
+    }
 }
